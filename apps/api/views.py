d79549fb9e8774c0bbdf0a66a036645e4cddced1
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-07-30 16:09:42 +0100

    chore:Product Category Serializer Implemented

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from requests import Response
+from rest_framework import  decorators, permissions, response, status, viewsets
 
-# Create your views here.
+from .serializers import ProductCategorySerializer
+
+from apps.product.models import (Product, ProductAttribute, ProductAttributeValue,
+                                 ProductCategory, ProductImage, ProductStore, ProductType)
+
+class AllProductCategory(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductCategorySerializer()
+
+@decorators.api_view(['GET'])
+def all_category_list_view(request):
+    if request.method == 'GET':
+        product_cat = ProductCategory.objects.all()
+        serializer = ProductCategory(product_cat, many=True)
+        return response.Response(serializer.data)
+
+@decorators.api_view(['GET', 'PUT'])
+def category_detail(request, pk):
+    try:
+        category = ProductCategory.objects.get(id=pk)
+    except ProductCategory.DoesNotExist:
+        return response.Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductCategorySerializer(category)
+        return response.Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductCategorySerializer(category, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@decorators.api_view(['DELETE'])
+def category_modify_view(request, pk):
+    """
+        Restricted Permissions => only Admins.. Other permission can be Implemented
+    """
+    try:
+        category = ProductCategory.objects.get(id=pk)
+    except ProductCategory.DoesNotExist:
+        return response.Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        category.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
