06a6d744f1eaebb154f5c94435e217c1e400af29
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-06 14:46:42 +0100

    chore:update the serialized class

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -9,13 +9,38 @@ class BrandSerializer(serializers.ModelSerializer):
         model = Brand
         fields = ['name']
 
+class ProductAttributeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductAttributeValue
+        exclude = ['pkid', 'id']
+
+
 class ProductCategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ProductCategory
-        fields = '__all__'
+        exclude = ['']
+
+
+class ProductAttributeValueSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductAttributeValue
+        exclude = ['pkid', 'id']
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class ProductInventorySerializer(serializers.ModelSerializer):
+    brand = BrandSerializer(many=False, read_only=True)
+    product_attribute = ProductAttributeValueSerializer(many=True, read_only=True)
+
+    class Meta:
+        models = ProductStore
+        fields = ('sku', 'pc', 'product_type', 'product', 'brand', 'attribute_val', 'retail_price', 'product_att_val', 'store_price', 'sale_price', 'discount_percentage', 'weight', 'weight_type')
+
