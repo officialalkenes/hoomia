1f4defad3c50a70b3506c694a8600f2ddb33fa5d
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-09 17:30:19 +0100

    chore: exploring restframework authentication options

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -2,19 +2,37 @@ from rest_framework import permissions
 
 
 class IsVendorPermissions(permissions.DjangoModelPermissions):
+    # def has_permission(self, request, view):
+    #     user = request.user
+    #     if request.user.is_vendor():
+    #         if user.has_perm('products.add_product'):
+    #             return True
+    #         if user.has_perm('products.change_product'):
+    #             return True
+    #         if user.has_perm('products.delete_product'):
+    #             return True
+    #         if user.has_perm('products.view_product'):
+    #             return True
+    #         return True
+    #     return False
+
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+
     def has_permission(self, request, view):
         user = request.user
-        if request.user.is_vendor():
-            if user.has_perm('product.add_product'):
-                return True
-            if user.has_perm('product.change_product'):
-                return True
-            if user.has_perm('product.delete_product'):
-                return True
-            if user.has_perm('product.view_product'):
-                return True
-            return True
-        return False
+        if not user.is_vendor:
+            return False
+        return super().has_permission(request, view)
+
 
     def has_object_permission(self, request, view, obj):
         return super().has_object_permission(request, view, obj)
