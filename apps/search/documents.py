c63d951526fe833bca555e30b346c487bd195613
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-08-09 14:57:58 +0100

    chore: exploring restframework views option

--- apps/search/documents.py
+++ apps/search/documents.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
 from apps.product.models import ProductStore, Product, ProductCategory
+
+
 
 @registry.register_document
 class ProductInventoryDocument(Document):
@@ -13,10 +16,13 @@ class ProductInventoryDocument(Document):
 
     class Index:
         name = 'productinventory'
-
+        settings = {'number_of_shards' : 1,
+                    'number_of_replicas': 0
+                    }
     class Django:
         model = ProductStore
         fields = [
             'id',
             'price',
         ]
+        
