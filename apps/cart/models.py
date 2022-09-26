26f7d2917ad7f7735fb4f151e9d14797601a1f05
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-09-26 19:57:48 +0100

    chore: added total price property on cart

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -9,16 +9,6 @@ from apps.product.models import Product
 User = get_user_model()
 
 
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
-    order = models.BooleanField(default=False)
-    total_price = models.DecimalField(decimal_places=2, max_digits=7, default=0.00)
-
-    class Meta:
-        verbose_name = _("Cart")
-        verbose_name_plural = _("Carts")
-
-
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.BooleanField(default=False)
@@ -26,3 +16,20 @@ class CartItem(models.Model):
         verbose_name = _("Cart Item")
         verbose_name_plural = _("Cart Items")
 
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    cart_item = models.ManyToManyField(CartItem)
+    order = models.BooleanField(default=False)
+    total_price = models.DecimalField(decimal_places=2, max_digits=7, default=0.00)
+
+    class Meta:
+        verbose_name = _("Cart")
+        verbose_name_plural = _("Carts")
+
+    @property
+    def total_cart_price(self):
+        price = [0 + x.product__price for x in self.cart_item]
+        return price
+    
